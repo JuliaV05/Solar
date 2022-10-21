ce3e42a45ed7818ac2ffa7ef69ac6402188e058b
Júlia Viana <juliavianamendes05@gmail.com>
2022-10-21 15:48:55 -0300

    requisito 1 desenvolvido

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    retu n (<p>Sistema Solar</p>);
+    return (
+      <>
+        <p>Sistema Solar</p>
+        <Header />
+      </>
+    );
   }
 }
 
